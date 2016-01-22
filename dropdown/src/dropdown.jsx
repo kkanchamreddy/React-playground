6cd00360a75f57f1fe1844f0df36a98dec4dc67c
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-21 22:53:23 -0800

    Dropdown complete

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -4,7 +4,14 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function(){
-    console.log('Clciked');
+    this.setState({open: !this.state.open});
+  },
+
+  handleItemClick: function(item){
+    this.setState({
+      open: false,
+      itemTitle: item
+    })
   },
 
   getInitialState: function(){
@@ -13,13 +20,17 @@ module.exports = React.createClass({
 
   render: function(){
     var list = this.props.items.map(function(item){
-      return <ListItem item= {item} />
-    });
+      return <ListItem
+              item = {item}
+              whenItemClicked={this.handleItemClick}
+              className ={this.state.itemTitle === item ? "active" : ""}
+              />
+    }.bind(this));
 
     return <div className="dropdown">
       <Button clickHandler ={this.handleClick}
               className="btn-default"
-              title ={this.props.title}
+              title ={this.state.itemTitle || this.props.title}
               subTitleClassName="caret"/>
 
       <ul className={"dropdown-menu " + (this.state.open ? "show": "false")}>
--- dropdown/src/list-item.jsx
+++ dropdown/src/list-item.jsx
@@ -1,7 +1,10 @@
 var React = require('React');
 
 module.exports = React.createClass({
+  handleClick: function(){
+    this.props.whenItemClicked(this.props.item);
+  },
   render: function(){
-    return <li> <a> {this.props.item}</a></li>
+    return <li className = {this.props.className}> <a onClick={this.handleClick}> {this.props.item}</a></li>
   }
 });
